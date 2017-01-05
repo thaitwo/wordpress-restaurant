8b90f7888203f5c3dfe9874841f6b80fecb6d06c
thaitwo <tmtu90@gmail.com>
2017-01-04 22:09:38 -0800

    created menu section for custom fields

--- template-parts/content-page-menu.php
+++ template-parts/content-page-menu.php
@@ -15,48 +15,92 @@
 
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-                    <div class="entry-content l-flex is-hori">
-                        <div>
-                            <h1 class="menu-category">
-                                <?php
-                                    $menu_category = get_field('menu_category_left');
-                                    echo $menu_category;
-                                ?>
-                            </h1>
-                            <p class="menu-item">
-                                <?php
-                                    $menu_item = get_field('menu_item_left');
-                                    echo $menu_item;
-                                ?>
-                            </p>
-                            <p class="menu-item-description">
-                                <?php
-                                    $menu_item_description = get_field('menu_item_description_left');
-                                    echo $menu_item_description;
-                                ?>
-                            </p>
+                    <div class="entry-content">
+                        <div class="grid col-6">
+
+
+
+                            <?php
+
+                            // parent loop
+                            if( have_rows('menu_section') ):
+
+                                while( have_rows('menu_section') ) : the_row();
+
+                                    // vars
+                                    $section_image = get_sub_field('section_image');
+                                    $section_image_subtitle = get_sub_field('section_image_subtitle');
+                                    $section_image_title = get_sub_field('section_image_title');
+                                    $stars = get_sub_field('stars');
+                                    $menu_category = get_sub_field('menu_category');
+
+                                    ?>
+
+                                    <h1 class="menu-category">
+                                        <?php
+                                            echo $menu_category;
+                                        ?>
+                                    </h1>
+
+                                    <?php
+
+                                    // child loop
+                                    if( have_rows('menu_list') ):
+
+                                        while( have_rows('menu_list') ) : the_row();
+
+                                            // vars
+                                            $menu_item = get_sub_field('menu_item');
+                                            $menu_item_des = get_sub_field('menu_item_des');
+                                            $menu_item_price = get_sub_field('menu_item_price');
+
+                                            ?>
+
+
+                                            <p class="menu-item">
+                                                <?php
+                                                    echo $menu_item;
+                                                ?>
+                                            </p>
+
+                                            <p class="menu-item-des">
+                                                <?php
+                                                    echo $menu_item_des;
+                                                ?>
+                                            </p>
+
+                                        <?php
+                                        endwhile;
+
+                                    endif;
+                                    // end child loop
+
+                                endwhile;
+
+                            endif;
+                            // end parent loop
+
+                            ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                         </div>
 
-                        <div>
-                            <h1 class="menu-category">
-                                <?php
-                                    $menu_category = get_field('menu_category_r');
-                                    echo $menu_category;
-                                ?>
-                            </h1>
-                            <p class="menu-item">
-                                <?php
-                                    $menu_item = get_field('menu_item_r');
-                                    echo $menu_item;
-                                ?>
-                            </p>
-                            <p class="menu-item-description">
-                                <?php
-                                    $menu_item_description = get_field('menu_item_description_r');
-                                    echo $menu_item_description;
-                                ?>
-                            </p>
-                        </div>
 
                         <div class="clearfix"></div>
 
@@ -73,7 +117,7 @@
 
 
 
-        <div class="section-divider full l-flex is-hori align-vert-center" style="background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url( <?php echo the_field('section_1_divider_bg_img'); ?>) fixed; background-size: cover;">
+        <div class="section-divider full l-flex is-hori align-vert-center" style="background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url( <?php echo the_post_thumbnail_url(); ?>) fixed; background-size: cover;">
             <div class="section-divider-text">
                 <div class="section-divider-subtitle">
                     <?php $section_subtitle = get_field( 'section_1_divider_subtitle' );
@@ -95,7 +139,7 @@
                         }
                     ?>
                 </div>
-                <img src="wp-content/themes/dojo/assets/stars.png" class="stars-front">
+                <img src="../wp-content/themes/dojo/assets/stars.png" class="stars-front">
             </div>
         </div>
 
@@ -120,7 +164,6 @@
                                     echo $section2_title;
                                 ?>
                             </h1>
-                            <img class="stars-front" src="wp-content/themes/dojo/assets/stars.png">
                             <p class="section-description">
                                 <?php
                                     $section2_text = get_field('section_2_text');
@@ -135,9 +178,6 @@
                                 </a>
                             <?php
                             endif; ?>
-                        </div>
-                        <div class="front-content">
-                            <div class="section-image is-rounded l-left " style="background: url( <?php echo the_field('section_2_image'); ?>); background-size: cover;"></div>
                         </div>
 
                         <div class="clearfix"></div>
