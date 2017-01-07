c6f2fded0a0ffe3193a38e8896fa3354f41949da
thaitwo <tmtu90@gmail.com>
2017-01-06 16:54:29 -0800

    added repeater loops on menu template

--- template-parts/content-page-menu.php
+++ template-parts/content-page-menu.php
@@ -9,204 +9,251 @@
 
 ?>
 
-<div class="main-content-area full">
-        <div class="main-page">
-            <div id="content" class="site-content inner">
-
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-                    <div class="entry-content">
-                        <div class="grid col-6">
 
 
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-                            <?php
+            <div class="entry-content">
 
-                            // parent loop
-                            if( have_rows('menu_section') ):
+                    <?php
 
-                                while( have_rows('menu_section') ) : the_row();
+                    // parent loop
+                    if( have_rows('menu_section') ):
 
-                                    // vars
-                                    $section_image = get_sub_field('section_image');
-                                    $section_image_subtitle = get_sub_field('section_image_subtitle');
-                                    $section_image_title = get_sub_field('section_image_title');
-                                    $stars = get_sub_field('stars');
-                                    $menu_category = get_sub_field('menu_category');
+                        while( have_rows('menu_section') ) : the_row();
 
-                                    ?>
+                            // vars
+                            $section_image = get_sub_field('section_image');
+                            $section_image_subtitle = get_sub_field('section_image_subtitle');
+                            $section_image_title = get_sub_field('section_image_title');
+                            $stars = get_sub_field('stars');
 
-                                    <h1 class="menu-category">
-                                        <?php
-                                            echo $menu_category;
+                            ?>
+
+                            <div class="menu-section grid">
+
+                                <?php
+                                if ($section_image): ?>
+                                    <div class="section-divider full l-flex is-hori align-vert-center" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url( <?php echo $section_image; ?>) fixed; background-size: cover;">
+                                        <div class="section-divider-text">
+                                            <div class="section-divider-subtitle">
+                                                <?php
+                                                    if ($section_image_subtitle) {
+                                                        echo $section_image_subtitle;
+                                                    }
+                                                    else {
+                                                        echo " ";
+                                                    }
+                                                ?>
+                                            </div>
+                                            <div class="section-divider-title">
+                                                <?php
+                                                    if ($section_image_title) {
+                                                        echo $section_image_title;
+                                                    }
+                                                    else {
+                                                        echo " ";
+                                                    }
+                                                ?>
+                                            </div>
+                                            <img src="../wp-content/themes/dojo/assets/stars.png" class="stars-front">
+                                        </div>
+                                    </div>
+                                <?php
+                                endif;
+                                ?>
+
+
+                                <?php
+
+                                // child loop
+                                if( have_rows('menu_category_container_l') ):
+
+                                    while( have_rows('menu_category_container_l') ) : the_row();
+
+                                        // vars
+                                        $menu_category_l = get_sub_field('menu_category_l');
+
                                         ?>
-                                    </h1>
 
-                                    <?php
+                                        <div class="col-6">
 
-                                    // child loop
-                                    if( have_rows('menu_list') ):
+                                            <h1 class="menu-category">
+                                                <?php
+                                                    if ($menu_category_l) {
+                                                        echo $menu_category_l;
+                                                    }
+                                                    elseif (!$menu_category_l && $menu_category_r) {
+                                                        echo '<br />';
+                                                    }
+                                                    else {
+                                                        echo " ";
+                                                    }
+                                                ?>
+                                            </h1>
 
-                                        while( have_rows('menu_list') ) : the_row();
+                                            <?php
 
-                                            // vars
-                                            $menu_item = get_sub_field('menu_item');
-                                            $menu_item_des = get_sub_field('menu_item_des');
-                                            $menu_item_price = get_sub_field('menu_item_price');
+                                            // grandchild loop
+                                            if( have_rows('menu_list_l') ):
 
+                                                while( have_rows('menu_list_l') ) : the_row();
+
+                                                    // vars
+                                                    $menu_item_l = get_sub_field('menu_item_l');
+                                                    $menu_item_des_l = get_sub_field('menu_item_des_l');
+                                                    $menu_item_price_l = get_sub_field('menu_item_price_l');
+
+                                                    ?>
+
+                                                    <div class="menu-list-container">
+                                                        <p class="menu-list-item">
+                                                            <?php echo $menu_item_l; ?>
+                                                        </p>
+
+                                                        <p class="menu-list-item-price">
+                                                            <?php echo $menu_item_price_l; ?>
+                                                        </p>
+
+                                                        <p class="menu-list-item-des">
+                                                            <?php echo $menu_item_des_l; ?>
+                                                        </p>
+                                                    </div>
+
+
+                                                <?php
+                                                endwhile;
+
+                                            endif;
+                                            // end grandchild loop
                                             ?>
 
+                                        </div>
 
-                                            <p class="menu-item">
+                                    <?php
+                                    endwhile;
+
+                                endif;
+                                // end child loop
+                                ?>
+
+
+                                <?php
+
+                                // child loop
+                                if( have_rows('menu_category_container_r') ):
+
+                                    while( have_rows('menu_category_container_r') ) : the_row();
+
+                                        // vars
+                                        $menu_category_r = get_sub_field('menu_category_r');
+
+                                        ?>
+
+                                        <div class="col-6">
+
+                                            <h1 class="menu-category">
                                                 <?php
-                                                    echo $menu_item;
+                                                    if ($menu_category_r) {
+                                                        echo $menu_category_r;
+                                                    }
+                                                    elseif (!$menu_category_r && $menu_category_l) {
+                                                        echo '<br />';
+                                                    }
+                                                    else {
+                                                        echo " ";
+                                                    }
                                                 ?>
-                                            </p>
+                                            </h1>
 
-                                            <p class="menu-item-des">
+                                            <?php
+
+                                            // grandchild loop
+                                            if( have_rows('menu_list_r') ):
+
+                                                while( have_rows('menu_list_r') ) : the_row();
+
+                                                    // vars
+                                                    $menu_item_r = get_sub_field('menu_item_r');
+                                                    $menu_item_des_r = get_sub_field('menu_item_des_r');
+                                                    $menu_item_price_r = get_sub_field('menu_item_price_r');
+
+                                                    ?>
+
+
+                                                    <div class="menu-list-container">
+                                                        <p class="menu-list-item">
+                                                            <?php echo $menu_item_r; ?>
+                                                        </p>
+
+                                                        <p class="menu-list-item-price">
+                                                            <?php echo $menu_item_price_r; ?>
+                                                        </p>
+
+                                                        <p class="menu-list-item-des">
+                                                            <?php echo $menu_item_des_r; ?>
+                                                        </p>
+                                                    </div>
+
                                                 <?php
-                                                    echo $menu_item_des;
-                                                ?>
-                                            </p>
+                                                endwhile;
 
-                                        <?php
-                                        endwhile;
+                                            endif;
+                                            // end grandchild loop
+                                            ?>
 
-                                    endif;
-                                    // end child loop
+                                        </div>
 
-                                endwhile;
+                                    <?php
+                                    endwhile;
 
-                            endif;
-                            // end parent loop
+                                endif;
+                                // end child loop
+                                ?>
 
-                            ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                        </div>
-
-
-                        <div class="clearfix"></div>
+                            </div>
 
                         <?php
-                            wp_link_pages( array(
-                                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dojo' ),
-                                'after'  => '</div>',
-                            ) );
-                        ?>
-                    </div><!-- .entry-content -->
-                </article><!-- #post-## -->
-            </div><!-- #content -->
-        </div>
+                        endwhile;
 
-
-
-        <div class="section-divider full l-flex is-hori align-vert-center" style="background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url( <?php echo the_post_thumbnail_url(); ?>) fixed; background-size: cover;">
-            <div class="section-divider-text">
-                <div class="section-divider-subtitle">
-                    <?php $section_subtitle = get_field( 'section_1_divider_subtitle' );
-                        if ($section_subtitle) {
-                            echo $section_subtitle;
-                        }
-                        else {
-                            echo "Welcome";
-                        }
+                    endif;
+                    // end parent loop
                     ?>
-                </div>
-                <div class="section-divider-title">
-                    <?php $section_title = get_field( 'section_1_divider_title' );
-                        if ($section_title) {
-                            echo $section_title;
-                        }
-                        else {
-                            echo bloginfo( 'name' );
-                        }
+
+
+
+
+
+
+
+                <div class="clearfix"></div>
+
+                <?php
+                    wp_link_pages( array(
+                        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dojo' ),
+                        'after'  => '</div>',
+                    ) );
+                ?>
+            </div><!-- .entry-content -->
+
+
+
+
+            <?php if ( get_edit_post_link() ) : ?>
+                <footer class="entry-footer">
+                    <?php
+                        edit_post_link(
+                            sprintf(
+                                /* translators: %s: Name of current post */
+                                esc_html__( 'Edit %s', 'dojo' ),
+                                the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                            ),
+                            '<span class="edit-link">',
+                            '</span>'
+                        );
                     ?>
-                </div>
-                <img src="../wp-content/themes/dojo/assets/stars.png" class="stars-front">
-            </div>
-        </div>
+                </footer><!-- .entry-footer -->
+            <?php endif; ?>
 
+        </article><!-- #post-## -->
 
-
-        <div class="main-page">
-            <div id="content" class="site-content inner">
-
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-                    <div class="entry-content l-flex is-hori-rev align-vert-center">
-                        <div class="front-content">
-                            <h2 class="section-subtitle">
-                                <?php
-                                    $section2_subtitle = get_field('section_2_subtitle');
-                                    echo $section2_subtitle;
-                                ?>
-                            </h2>
-                            <h1 class="section-title">
-                                <?php
-                                    $section2_title = get_field('section_2_title');
-                                    echo $section2_title;
-                                ?>
-                            </h1>
-                            <p class="section-description">
-                                <?php
-                                    $section2_text = get_field('section_2_text');
-                                    echo $section2_text;
-                                ?>
-                            </p>
-
-                            <?php $section_2_button = get_field( 'section_2_button_text' );
-                            if ($section_2_button) : ?>
-                                <a class="button button-secondary" href="<?php echo the_field('section_2_button_link') ?>">
-                                <?php echo $section_2_button; ?>
-                                </a>
-                            <?php
-                            endif; ?>
-                        </div>
-
-                        <div class="clearfix"></div>
-
-                        <?php
-                            wp_link_pages( array(
-                                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dojo' ),
-                                'after'  => '</div>',
-                            ) );
-                        ?>
-                    </div><!-- .entry-content -->
-
-                    <?php if ( get_edit_post_link() ) : ?>
-                        <footer class="entry-footer">
-                            <?php
-                                edit_post_link(
-                                    sprintf(
-                                        /* translators: %s: Name of current post */
-                                        esc_html__( 'Edit %s', 'dojo' ),
-                                        the_title( '<span class="screen-reader-text">"', '"</span>', false )
-                                    ),
-                                    '<span class="edit-link">',
-                                    '</span>'
-                                );
-                            ?>
-                        </footer><!-- .entry-footer -->
-                    <?php endif; ?>
-
-                </article><!-- #post-## -->
-            </div><!-- #content -->
-        </div>
-    </div>
