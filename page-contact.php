5a0da78227ecd67f91a6bcf00fe9336fa11d10a1
thaitwo <tmtu90@gmail.com>
2017-01-16 02:11:11 -0800

    converted indentation to spaces

--- page-contact.php
+++ page-contact.php
@@ -7,20 +7,20 @@ Template Name: Contact Template
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+            <?php
+            while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page-contact' ); ?>
+                get_template_part( 'template-parts/content', 'page-contact' ); ?>
 
-			<?php
-			endwhile; // End of the loop.
-			?>
+            <?php
+            endwhile; // End of the loop.
+            ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
 get_footer();
--- template-parts/content-page-contact.php
+++ template-parts/content-page-contact.php
@@ -9,41 +9,41 @@
 
 ?>
 <div class="main-content-area full">
-	<div class="main-page">
+    <div class="main-page">
 
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<div class="entry-content">
+            <div class="entry-content">
 
-				<div class="contact-container">
-					<?php
-						the_content();
-					?>
+                <div class="contact-container">
+                    <?php
+                        the_content();
+                    ?>
 
-			        <div class="section-image is-rounded l-right" style="background: url( <?php echo the_field('contact_image'); ?>); background-size: cover;"></div>
-				</div>
+                    <div class="section-image is-rounded l-right" style="background: url( <?php echo the_field('contact_image'); ?>); background-size: cover;"></div>
+                </div>
 
-		        <?php
-					wp_link_pages( array(
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dojo' ),
-						'after'  => '</div>',
-					) );
-				?>
-			</div><!-- .entry-content -->
+                <?php
+                    wp_link_pages( array(
+                        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dojo' ),
+                        'after'  => '</div>',
+                    ) );
+                ?>
+            </div><!-- .entry-content -->
 
-			<?php if ( get_edit_post_link() ) : ?>
-				<footer class="entry-footer">
-					<?php
-						edit_post_link(
-							sprintf(
-								/* translators: %s: Name of current post */
-								esc_html__( 'Edit %s', 'dojo' ),
-								the_title( '<span class="screen-reader-text">"', '"</span>', false )
-							),
-							'<span class="edit-link">',
-							'</span>'
-						);
-					?>
-				</footer><!-- .entry-footer -->
-			<?php endif; ?>
-		</article><!-- #post-## -->
+            <?php if ( get_edit_post_link() ) : ?>
+                <footer class="entry-footer">
+                    <?php
+                        edit_post_link(
+                            sprintf(
+                                /* translators: %s: Name of current post */
+                                esc_html__( 'Edit %s', 'dojo' ),
+                                the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                            ),
+                            '<span class="edit-link">',
+                            '</span>'
+                        );
+                    ?>
+                </footer><!-- .entry-footer -->
+            <?php endif; ?>
+        </article><!-- #post-## -->
