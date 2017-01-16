24bf43bfcc8602b2217146d507c8a42458c79dac
thaitwo <tmtu90@gmail.com>
2017-01-16 01:58:33 -0800

    new branch form to replace contact branch due to conflicts

---
+++ template-parts/content-page-contact.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Template part for displaying page content in page-contact.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Dojo
+ */
+
+?>
+<div class="main-content-area full">
+	<div class="main-page">
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+			<div class="entry-content">
+
+				<div class="contact-container">
+					<?php
+						the_content();
+					?>
+
+			        <div class="section-image is-rounded l-right" style="background: url( <?php echo the_field('contact_image'); ?>); background-size: cover;"></div>
+				</div>
+
+		        <?php
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dojo' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+
+			<?php if ( get_edit_post_link() ) : ?>
+				<footer class="entry-footer">
+					<?php
+						edit_post_link(
+							sprintf(
+								/* translators: %s: Name of current post */
+								esc_html__( 'Edit %s', 'dojo' ),
+								the_title( '<span class="screen-reader-text">"', '"</span>', false )
+							),
+							'<span class="edit-link">',
+							'</span>'
+						);
+					?>
+				</footer><!-- .entry-footer -->
+			<?php endif; ?>
+		</article><!-- #post-## -->
